92bd1e340756bb8f6fca3c8e765bb9acd80a2f77
Vladimir <botdes@botland.(none)>
2010-01-06 22:16:28 +0300

    Main class

---
+++ src/com/botland/backrabbit/Main.java
@@ -0,0 +1,12 @@
+package com.botland.backrabbit;
+
+/**
+ * Author: Vladimir Batygin
+ * Date: 06.01.2010
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Back Rabbit!");
+    }
+}
