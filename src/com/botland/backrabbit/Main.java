4406260fa12bd3be6f5749b03f53a3ff06e97f00
Vladimir <botdes@botland.(none)>
2010-01-06 22:20:54 +0300

    View

--- src/com/botland/backrabbit/Main.java
+++ src/com/botland/backrabbit/Main.java
@@ -1,5 +1,7 @@
 package com.botland.backrabbit;
 
+import com.botland.backrabbit.view.View;
+
 /**
  * Author: Vladimir Batygin
  * Date: 06.01.2010
@@ -8,5 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, I am Back Rabbit!");
+        View view = new View();
+        view.setVisible(true);
     }
 }
---
+++ src/com/botland/backrabbit/view/View.java
@@ -0,0 +1,17 @@
+package com.botland.backrabbit.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Author: Vladimir Batygin
+ * Date: 06.01.2010
+ */
+public class View extends JFrame {
+
+
+    public View() throws HeadlessException {
+        setSize(new Dimension(600, 600));        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
