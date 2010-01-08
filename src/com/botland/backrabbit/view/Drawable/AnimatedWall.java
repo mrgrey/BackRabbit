3e6097b536975758bbb4bef4e545c8959bc286de
Vladimir <botdes@botland.(none)>
2010-01-08 17:10:37 +0300

    filling images

--- src/com/botland/backrabbit/view/Drawable/AnimatedWall.java
+++ src/com/botland/backrabbit/view/Drawable/AnimatedWall.java
@@ -5,6 +5,7 @@ import com.botland.backrabbit.util.Position;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
 /**
@@ -26,8 +27,25 @@ public class AnimatedWall extends AbstractAnimatedObject {
     @Override
     public void paint(final Graphics g) {
         //  g.drawImage(image, wall.getPosition().getX(), wall.getPosition().getY(), this);
-        drawImagesInRect(g, image);
+        //drawImagesInRect(g, image);
         //  g.setColor(Color.BLACK);
+        //  final BufferedImage bufferedImage = new BufferedImage(10, 10, BufferedImage.TRANSLUCENT);
+//        bufferedImage.
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(this), image.getHeight(this), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.drawImage(image, 0, 0, this);
+
+
+        final TexturePaint paint = new TexturePaint(bufferedImage, new Rectangle(0, 0, image.getWidth(this), image.getHeight(this)));
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint(paint);
+        final Rectangle s = new Rectangle(wall.getPosition().getX(), wall.getPosition().getY(), wall.getWidth(), wall.getHeight());
+        g2.fill(s);
+
+        g2d.setPaint(Color.blue);
+        g2d.setStroke(new BasicStroke(5));
+        g2d.draw(s);
+
         g.setColor(Color.GREEN);
         g.drawRect(wall.getPosition().getX(), wall.getPosition().getY(), wall.getWidth(), wall.getHeight());
     }
